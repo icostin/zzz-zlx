84766f5ac5fcf42ed3b46b02508c67ae98b1b617
Costin Ionescu <costin.ionescu@gmail.com>
2016-03-28 07:41:50 -0700

    added thread create/join and dummy interfaces

--- thread.c
+++ thread.c
@@ -1,10 +1,12 @@
 #include "zlx/thread.h"
+#include "zlx/assert.h"
 
-/* zlx_nop_mutex_op *********************************************************/
-ZLX_API void ZLX_CALL zlx_nop_mutex_op (zlx_mutex_t * mutex_p)
+/* zlx_nosup_thread_xfc *****************************************************/
+ZLX_API zlx_thread_xfc_t zlx_nosup_thread_xfc =
 {
-    (void) mutex_p;
-}
+    zlx_nosup_thread_create,
+    zlx_nosup_thread_join
+};
 
 /* zlx_nop_mutex_xfc ********************************************************/
 ZLX_API zlx_mutex_xfc_t zlx_nop_mutex_xfc =
@@ -15,3 +17,67 @@ ZLX_API zlx_mutex_xfc_t zlx_nop_mutex_xfc =
     zlx_nop_mutex_op,
     1
 };
+
+/* zlx_nosup_cond_xfc *******************************************************/
+ZLX_API zlx_cond_xfc_t zlx_nosup_cond_xfc =
+{
+    zlx_nosup_cond_init,
+    zlx_nop_cond_op,
+    zlx_nop_cond_op,
+    zlx_nop_cond_wait,
+    1
+};
+
+/* zlx_nosup_thread_create **************************************************/
+ZLX_API unsigned int ZLX_CALL zlx_nosup_thread_create
+    (
+        zlx_tid_t * tid_p,
+        zlx_thread_func_t func,
+        void * arg
+    )
+{
+    (void) tid_p, (void) func, (void) arg;
+    return ZLX_THSTAT_NO_SUP;
+}
+
+/* zlx_nosup_thread_join ****************************************************/
+ZLX_API unsigned int ZLX_CALL zlx_nosup_thread_join
+    (
+        zlx_tid_t tid,
+        uint8_t * ret_val_p
+    )
+{
+    (void) tid;
+    (void) ret_val_p;
+    return ZLX_THSTAT_NO_SUP;
+}
+
+/* zlx_nop_mutex_op *********************************************************/
+ZLX_API void ZLX_CALL zlx_nop_mutex_op (zlx_mutex_t * mutex_p)
+{
+    (void) mutex_p;
+}
+
+/* zlx_nosup_cond_init ******************************************************/
+ZLX_API zlx_thstat_t ZLX_CALL zlx_nosup_cond_init (zlx_cond_t * cond_p)
+{
+    (void) cond_p;
+    return ZLX_THSTAT_NO_SUP;
+}
+
+/* zlx_nop_cond_op **********************************************************/
+ZLX_API void ZLX_CALL zlx_nop_cond_op (zlx_cond_t * cond_p)
+{
+    (void) cond_p;
+}
+
+/* zlx_nop_cond_wait ********************************************************/
+ZLX_API void ZLX_CALL zlx_nop_cond_wait
+    (
+        zlx_cond_t * cond_p,
+        zlx_mutex_t * mutex_p
+    )
+{
+    (void) cond_p, (void) mutex_p;
+}
+
--- zlx/thread.h
+++ zlx/thread.h
@@ -3,9 +3,58 @@
 
 #include "base.h"
 
+typedef enum zlx_thstat_enum zlx_thstat_t;
+typedef uintptr_t zlx_tid_t;
+typedef uint_fast8_t (ZLX_CALL * zlx_thread_func_t) (void * arg);
 typedef struct zlx_mutex_s zlx_mutex_t;
 typedef struct zlx_cond_s zlx_cond_t;
+
+typedef struct zlx_thread_xfc_s zlx_thread_xfc_t;
 typedef struct zlx_mutex_xfc_s zlx_mutex_xfc_t;
+typedef struct zlx_cond_xfc_s zlx_cond_xfc_t;
+typedef struct zlx_tmc_xfc_s zlx_tmc_xfc_t;
+
+enum zlx_thstat_enum
+{
+    ZLX_THSTAT_OK = 0,
+    ZLX_THSTAT_FAILED, // generic "failed" status
+    ZLX_THSTAT_NO_SUP, // feature not supported
+};
+
+struct zlx_thread_xfc_s
+{
+    /*  create  */
+    /**
+     *  Creates a thread.
+     *  @param tid_p [out]
+     *      receives the thread ID
+     *  @param func [in]
+     *      function to run in the created thread
+     *  @param arg [in]
+     *      argument for the function
+     *  @retval 0   all ok
+     *  @retval 1   failed
+     */
+    unsigned int (ZLX_CALL * create)
+        (
+            zlx_tid_t * tid_p,
+            zlx_thread_func_t func,
+            void * arg
+        );
+    /*  join  */
+    /**
+     *  Waits for the given thread to end.
+     *  @param tid [in]
+     *      thread to wait for
+     *  @param ret_val_p [out, opt]
+     *      receives the return value of the thread function
+     */
+    unsigned int (ZLX_CALL * join)
+        (
+            zlx_tid_t tid,
+            uint8_t * ret_val_p
+        );
+};
 
 struct zlx_mutex_xfc_s
 {
@@ -23,14 +72,44 @@ struct zlx_cond_xfc_s
      *  @retval 0 ok
      *  @retval 1 failed
      */
-    unsigned int (ZLX_CALL * init) (zlx_cond_t * cond_p);
+    zlx_thstat_t (ZLX_CALL * init) (zlx_cond_t * cond_p);
     void (ZLX_CALL * finish) (zlx_cond_t * cond_p);
     void (ZLX_CALL * signal) (zlx_cond_t * cond_p);
     void (ZLX_CALL * wait) (zlx_cond_t * cond_p, zlx_mutex_t * mutex_p);
     size_t size;
 };
 
+struct zlx_tmc_xfc_s
+{
+    zlx_thread_xfc_t thread;
+    zlx_mutex_xfc_t mutex;
+    zlx_cond_xfc_t cond;
+};
+
+ZLX_API unsigned int ZLX_CALL zlx_nosup_thread_create
+    (
+        zlx_tid_t * tid_p,
+        zlx_thread_func_t func,
+        void * arg
+    );
+
+ZLX_API unsigned int ZLX_CALL zlx_nosup_thread_join
+    (
+        zlx_tid_t tid,
+        uint8_t * ret_val_p
+    );
+
 ZLX_API void ZLX_CALL zlx_nop_mutex_op (zlx_mutex_t * mutex_p);
+ZLX_API zlx_thstat_t ZLX_CALL zlx_nosup_cond_init (zlx_cond_t * cond_p);
+ZLX_API void ZLX_CALL zlx_nop_cond_op (zlx_cond_t * cond_p);
+ZLX_API void ZLX_CALL zlx_nop_cond_wait
+    (
+        zlx_cond_t * cond_p,
+        zlx_mutex_t * mutex_p
+    );
+
+ZLX_API zlx_thread_xfc_t zlx_nosup_thread_xfc;
 ZLX_API zlx_mutex_xfc_t zlx_nop_mutex_xfc;
+ZLX_API zlx_cond_xfc_t zlx_nosup_cond_xfc;
 
 #endif /* _ZLX_THREAD_H */
